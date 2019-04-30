d1c052e98f9a4f8f944582cd2035b31077b2d7ae
Michal Piaskowski <mich.piaskowski@gmail.com>
2019-04-30 19:11:58 +0200

    Added logic to recipe-item

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   @Output() featureSelected = new EventEmitter<string>();
+
   constructor() { }
 
   ngOnInit() {
