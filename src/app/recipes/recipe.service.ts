b130c2a8b75b40b245af2fdb295c3d6b1e519ea9
Michal Piaskowski <mich.piaskowski@gmail.com>
2019-05-01 19:20:32 +0200

    Added ingredients services for cross component communication

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,23 +1,41 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
     new Recipe(
-      'A test recipe',
-      'This is simply a test',
-      'https://cdn.pixabay.com/photo/2015/08/21/18/19/spare-ribs-899306_960_720.jpg'
+      'A pasta',
+      'This is a simple pasta recipe',
+      'https://cdn.pixabay.com/photo/2018/07/18/19/12/spaghetti-3547078_960_720.jpg',
+      [
+        new Ingredient('Noodles', 100),
+        new Ingredient('Parmesan', 15),
+      ]
     ),
     new Recipe(
-      'Another test recipe',
-      'This is simply a test',
-      'https://cdn.pixabay.com/photo/2015/08/21/18/19/spare-ribs-899306_960_720.jpg'
+      'Pizza',
+      'Best homemade pizza there is',
+      'https://cdn.pixabay.com/photo/2016/04/09/09/22/pizza-1317699_960_720.jpg',
+      [
+        new Ingredient('Pizza dough', 200),
+        new Ingredient('Cheese', 50),
+      ]
     )
   ];
+
+  constructor(private slService: ShoppingListService) {}
+
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
