f2688c507f9b1976b8dfe303779cf1736afdc087
Michal Piaskowski <mich.piaskowski@gmail.com>
2019-05-03 15:25:01 +0200

    Added firebase and signup/signin user features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   loadedFeature = 'recipe';
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apikey: 'AIzaSyCiUYjm65Df4TDIgFUt4x14hDrMnZJK02U',
+      authDomain: 'ng-recipe-book-6ae37.firabaseapp.com'
+    })
+  }
 
   onNavigate(feature: string) {
     this.loadedFeature = feature;
